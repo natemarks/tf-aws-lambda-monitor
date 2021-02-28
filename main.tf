00244374a693f2c6e39d7faac7418638b0b70f78
Nate Marks <npmarks@gmail.com>
2021-02-28 06:06:18 -0500

    Add dependency lambda function -> cloudwatch log

--- main.tf
+++ main.tf
@@ -83,6 +83,10 @@ resource "aws_lambda_function" "this" {
     var.environment_variables,
     )
   }
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.this,
+  ]
 }
 
 
